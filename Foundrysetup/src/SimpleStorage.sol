691697e9d187a518686befff9d123e7f0e23ee16
adityaanandcodes <adityaanandatwork276@gmail.com>
2025-01-27 17:50:01 +0000

    learning to deploy smart contract through anvil

--- Foundrysetup/src/SimpleStorage.sol
+++ Foundrysetup/src/SimpleStorage.sol
@@ -1,16 +1,33 @@
+// I'm a comment!
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+
+pragma solidity 0.8.19;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
-    uint256 private data;
+    uint256 myFavoriteNumber;
 
-    // Function to set the value
-    function set(uint256 _data) public {
-        data = _data;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
     }
 
-    // Function to get the value
-    function get() public view returns (uint256) {
-        return data;
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
